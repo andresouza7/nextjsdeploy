492722d442736b384d690bf64b19eb5c5441cdfe
Andre Souza <souza.andre1993@gmail.com>
2021-01-30 03:02:09 -0300

    asdlfj

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const URL_LOCALHOST = 'http://localhost:3000'
-const URL_CLOUD = process.env.VERCEL_URL.split('/')[0]
+const URL_CLOUD = ''
 
 console.log(URL_CLOUD)
 const dev = process.env.NODE_ENV !== 'production'
